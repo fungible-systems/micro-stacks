e35a7c9077b74986b03e2a3e2f2b561fb862afc6
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-09-16 15:46:12 -0500

    chore: referrer only on client

--- src/common/fetch-private.ts
+++ src/common/fetch-private.ts
@@ -1,7 +1,11 @@
-const DEFAULT_FETCH_OPTIONS: RequestInit = {
-  referrer: 'no-referrer',
-  referrerPolicy: 'no-referrer',
-};
+const isClientSide = typeof window !== 'undefined';
+
+const DEFAULT_FETCH_OPTIONS: RequestInit = isClientSide
+  ? {
+      referrer: 'no-referrer',
+      referrerPolicy: 'no-referrer',
+    }
+  : {};
 
 export async function fetchPrivate(input: RequestInfo, init: RequestInit = {}): Promise<Response> {
   return fetch(input, { ...DEFAULT_FETCH_OPTIONS, ...init });
