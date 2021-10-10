73568e354a0e1cf5aaff931ea74c5a062205b9d6
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-10-10 18:19:47 -0400

    SmartContracts remaining fetchers and tests

--- src/react/api/smartcontracts/fetchers.ts
+++ src/react/api/smartcontracts/fetchers.ts
@@ -7,9 +7,20 @@ import {
   TransactionEventFungibleAsset,
   TransactionEventNonFungibleAsset,
   ContractSourceResponse,
+  ReadOnlyFunctionSuccessResponse,
+  MapEntryResponse,
 } from '@stacks/stacks-blockchain-api-types';
+import { cvToHex } from 'micro-stacks/clarity';
 import { BaseListParams } from '../types';
-import { fetchJson, generateUrl, contractEndpoint, contractsEndpoint } from '../utils';
+import {
+  fetchJson,
+  fetchJsonPost,
+  generateUrl,
+  contractEndpoint,
+  contractsEndpoint,
+  v2Endpoint,
+} from '../utils';
+import { ReadOnlyFunctionFetcherOptions } from './types';
 
 /**
  * Get contract info using the contract ID
@@ -77,6 +88,35 @@ export async function fetchContractInterface({
 }
 
 /**
+ * Get specific data-map inside a contract
+ *
+ * @see https://blockstack.github.io/stacks-blockchain-api/#operation/get_contract_data_map_entry
+ */
+export async function fetchContractDataMapEntry({
+  url,
+  contract_name,
+  contract_address,
+  map_name,
+  proof,
+  tip,
+  lookup_key,
+}: BaseListParams & {
+  contract_name: string;
+  contract_address: string;
+  map_name: string;
+  proof: number;
+  tip: string;
+  lookup_key: string;
+}) {
+  const body = lookup_key;
+  const path = generateUrl(
+    `${v2Endpoint(url)}/map_entry/${contract_address}/${contract_name}/${map_name}`,
+    { proof: proof, tip: tip }
+  );
+  return fetchJsonPost<MapEntryResponse>(path, body);
+}
+
+/**
  * Get contract source
  *
  * @see https://blockstack.github.io/stacks-blockchain-api/#operation/get_contract_source
@@ -102,4 +142,31 @@ export async function fetchContractSource({
     }
   );
   return fetchJson<ContractSourceResponse>(path);
+}
+
+/**
+ * Call a read-only public function on a given smart contract.
+ *
+ * @see https://blockstack.github.io/stacks-blockchain-api/#tag/fee_rate
+ */
+export async function fetchReadOnlyFunction({
+  url,
+  contractName,
+  contractAddress,
+  functionName,
+  functionArgs,
+  senderAddress,
+  tip,
+}: BaseListParams & ReadOnlyFunctionFetcherOptions) {
+  const args = functionArgs.map(arg => cvToHex(arg));
+
+  const body = JSON.stringify({
+    sender: senderAddress,
+    arguments: args,
+  });
+  const path = generateUrl(
+    `${contractsEndpoint(url)}/call-read/${contractAddress}/${contractName}/${functionName}`,
+    { tip: tip }
+  );
+  return fetchJsonPost<ReadOnlyFunctionSuccessResponse>(path, body);
 }
