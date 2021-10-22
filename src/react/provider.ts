60b03c65424dde2dd4921c62f497f33bce9920af
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-10-22 13:28:00 -0500

    chore: more cleanup

--- src/react/provider.ts
+++ src/react/provider.ts
@@ -12,6 +12,13 @@ import type { Atom } from 'jotai';
 
 type NetworkType = StacksNetwork | 'mainnet' | 'testnet' | 'mocknet';
 
+interface MicroStacksProviderProps {
+  authOptions: AuthOptions;
+  storageAdapter?: StorageAdapter<unknown>;
+  network?: NetworkType;
+  initialValues?: Iterable<readonly [Atom<unknown>, unknown]>;
+}
+
 function getNetwork(network?: NetworkType) {
   if (!network) return;
   switch (network) {
@@ -26,37 +33,26 @@ function getNetwork(network?: NetworkType) {
   }
 }
 
-interface MicroStacksProviderProps {
-  authOptions: AuthOptions;
-  storageAdapter?: StorageAdapter<unknown>;
-  network?: NetworkType;
-  initialValues?: Iterable<readonly [Atom<unknown>, unknown]>;
-}
-
 export const MicroStacksProvider: FC<MicroStacksProviderProps> = ({
   children,
   authOptions,
   storageAdapter = defaultStorageAdapter,
   network = new StacksMainnet(),
-  initialValues = [],
+  initialValues: additionalInitialValues = [],
 }) => {
   const networkValue = useMemo(() => getNetwork(network), [network]);
 
-  const microStacksInitialValues = useMemo(
+  const initialValues = useMemo(
     () => [
       [authOptionsAtom, authOptions] as const,
       [storageAdapterAtom, storageAdapter] as const,
       [networkValueAtom, networkValue] as const,
-      ...initialValues,
+      ...additionalInitialValues,
     ],
-    [authOptions, storageAdapter, networkValue, initialValues]
+    [authOptions, storageAdapter, networkValue, additionalInitialValues]
   );
 
-  return createElement(
-    Provider,
-    {
-      initialValues: microStacksInitialValues,
-    },
-    children
-  );
+  const props = useMemo(() => ({ initialValues }), [initialValues]);
+
+  return createElement(Provider, props, children);
 };
