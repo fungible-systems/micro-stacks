75b113d5648aee76455dfa3d22865af7d9cf6fd2
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-10-22 13:25:49 -0500

    chore: cleanup

--- src/react/provider.ts
+++ src/react/provider.ts
@@ -1,4 +1,4 @@
-import { FC, createElement } from 'react';
+import { FC, createElement, useMemo } from 'react';
 import { defaultStorageAdapter } from 'micro-stacks/connect';
 import { StacksNetwork, StacksMainnet, StacksTestnet, StacksMocknet } from 'micro-stacks/network';
 import { Provider } from 'jotai';
@@ -10,41 +10,48 @@ import { storageAdapterAtom } from './store/storage-adapter';
 import type { AuthOptions, StorageAdapter } from 'micro-stacks/connect';
 import type { Atom } from 'jotai';
 
-export const MicroStacksProvider: FC<{
+type NetworkType = StacksNetwork | 'mainnet' | 'testnet' | 'mocknet';
+
+function getNetwork(network?: NetworkType) {
+  if (!network) return;
+  switch (network) {
+    case 'mocknet':
+      return new StacksMocknet();
+    case 'testnet':
+      return new StacksTestnet();
+    case 'mainnet':
+      return new StacksMainnet();
+    default:
+      return network;
+  }
+}
+
+interface MicroStacksProviderProps {
   authOptions: AuthOptions;
   storageAdapter?: StorageAdapter<unknown>;
-  network?: StacksNetwork | 'mainnet' | 'testnet' | 'mocknet';
+  network?: NetworkType;
   initialValues?: Iterable<readonly [Atom<unknown>, unknown]>;
-}> = ({
+}
+
+export const MicroStacksProvider: FC<MicroStacksProviderProps> = ({
   children,
   authOptions,
   storageAdapter = defaultStorageAdapter,
   network = new StacksMainnet(),
   initialValues = [],
 }) => {
-  const getNetwork = () => {
-    if (network) {
-      if (typeof network === 'string') {
-        switch (network) {
-          case 'mocknet':
-            return new StacksMocknet();
-          case 'testnet':
-            return new StacksTestnet();
-          case 'mainnet':
-          default:
-            return new StacksMainnet();
-        }
-      }
-    }
-    return network;
-  };
-  const networkValue = getNetwork();
-  const microStacksInitialValues = [
-    [authOptionsAtom, authOptions] as const,
-    [storageAdapterAtom, storageAdapter] as const,
-    [networkValueAtom, networkValue] as const,
-    ...initialValues,
-  ];
+  const networkValue = useMemo(() => getNetwork(network), [network]);
+
+  const microStacksInitialValues = useMemo(
+    () => [
+      [authOptionsAtom, authOptions] as const,
+      [storageAdapterAtom, storageAdapter] as const,
+      [networkValueAtom, networkValue] as const,
+      ...initialValues,
+    ],
+    [authOptions, storageAdapter, networkValue, initialValues]
+  );
+
   return createElement(
     Provider,
     {
