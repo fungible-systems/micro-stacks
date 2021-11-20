934b642388b374708547c613a7b2ea909e0387b6
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-11-20 07:22:37 -0500

    fix: regtest has been permanently deprecated

--- src/crypto/base58/networks.ts
+++ src/crypto/base58/networks.ts
@@ -28,17 +28,6 @@ const bitcoin: Network = {
   scriptHash: 0x05,
   wif: 0x80,
 };
-const regtest: Network = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bcrt',
-  bip32: {
-    public: 0x043587cf,
-    private: 0x04358394,
-  },
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
-  wif: 0xef,
-};
 const testnet: Network = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
   bech32: 'tb',
