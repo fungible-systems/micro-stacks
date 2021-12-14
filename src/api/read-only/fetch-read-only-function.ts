d60c6d1e066eb48c988877e67daa4ffb2090f1ab
aulneau <aulneau@users.noreply.github.com>
2021-12-14 08:15:18 -0600

    fix: strictJsonCompat

--- src/api/read-only/fetch-read-only-function.ts
+++ src/api/read-only/fetch-read-only-function.ts
@@ -1,7 +1,6 @@
 import { callReadOnlyFunction } from './call-read-only-function';
 import { cvToTrueValue } from 'micro-stacks/clarity';
-
-import { ReadOnlyFunctionOptions } from './types';
+import type { ReadOnlyFunctionOptions } from './types';
 
 /**
  * Fetch and parse clarity read only function.
@@ -10,8 +9,14 @@ import { ReadOnlyFunctionOptions } from './types';
  *
  * Note: For best DX, pass the type to this function of the return value
  * @param options
+ * @param strictJsonCompat If true then ints and uints are returned as JSON serializable numbers when
+ * less than or equal to 53 bit length, otherwise string wrapped integers when larger than 53 bits.
+ * If false, they are returned as js native `bigint`s which are _not_ JSON serializable.
  */
-export async function fetchReadOnlyFunction<T>(options: ReadOnlyFunctionOptions): Promise<T> {
+export async function fetchReadOnlyFunction<T>(
+  options: ReadOnlyFunctionOptions,
+  strictJsonCompat?: boolean
+): Promise<T> {
   const value = await callReadOnlyFunction(options);
-  return cvToTrueValue<T>(value);
+  return cvToTrueValue<T>(value, strictJsonCompat);
 }
