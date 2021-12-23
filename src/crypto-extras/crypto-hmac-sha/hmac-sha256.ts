92fe0dfe7c0ceb0fdb257ec80e52b01d603c6b7b
aulneau <aulneau@users.noreply.github.com>
2021-12-23 17:53:55 -0600

    fix: imports

--- src/crypto-extras/crypto-hmac-sha/hmac-sha256.ts
+++ src/crypto-extras/crypto-hmac-sha/hmac-sha256.ts
@@ -1,7 +1,7 @@
 import { ensureUint8Array } from 'micro-stacks/common';
 
-import { hmac } from '@noble/hashes/lib/hmac';
-import { sha256 } from '@noble/hashes/lib/sha256';
+import { hmac } from '@noble/hashes/hmac';
+import { sha256 } from '@noble/hashes/sha256';
 
 export function hmacSha256(key: Uint8Array, ...messages: Uint8Array[]): Uint8Array {
   const hash = hmac.create(sha256, ensureUint8Array(key));
--- src/crypto-extras/crypto-sha/sha512.ts
+++ src/crypto-extras/crypto-sha/sha512.ts
@@ -1,4 +1,4 @@
-import { sha512 } from '@noble/hashes/lib/sha512';
+import { sha512 } from '@noble/hashes/sha512';
 import { ensureUint8Array } from 'micro-stacks/common';
 
 export function hashSha512(data: Uint8Array): Uint8Array {
