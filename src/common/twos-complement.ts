e5578a7ba6872391cf834948bbd80e1a108be9f4
aulneau <aulneau@users.noreply.github.com>
2022-03-08 13:23:33 -0600

    chore: add tests for twos functions

--- src/common/twos-complement.ts
+++ src/common/twos-complement.ts
@@ -1,20 +1,23 @@
 const _0n = BigInt(0);
 const _1n = BigInt(1);
-const _128n = BigInt(128);
 
-export function toTwos(value: bigint) {
+export function toTwos(value: bigint, bitlength: string | number = 128) {
   // make sure its in range given the number of bits
-  if (value < -(_1n << (_128n - _1n)) || value > (_1n << (_128n - _1n)) - _1n)
-    throw 'Integer out of range given 128 bits to represent.';
+  if (
+    value < -(_1n << (BigInt(bitlength) - _1n)) ||
+    value > (_1n << (BigInt(bitlength) - _1n)) - _1n
+  )
+    throw `Integer out of range given ${bitlength} bits to represent.`;
 
   // if positive, return the positive value
   if (value >= _0n) return value;
 
   // if negative, convert to twos complement representation
-  return ~((-value - _1n) | ~((_1n << _128n) - _1n));
+  return ~((-value - _1n) | ~((_1n << BigInt(bitlength)) - _1n));
 }
 
-export function fromTwos(value: bigint) {
-  if ((value & (_1n << (_128n - _1n))) > _0n) value = value - (_1n << _128n);
+export function fromTwos(value: bigint, bitlength: string | number = 128) {
+  if ((value & (_1n << (BigInt(bitlength) - _1n))) > _0n)
+    value = value - (_1n << BigInt(bitlength));
   return value;
 }
