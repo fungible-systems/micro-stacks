f485811b446497ddc6bf44954880aecddecbe30c
aulneau <aulneau@users.noreply.github.com>
2022-05-20 07:51:59 -0500

    fix: improve tests, remove extra logging

--- src/connect/message-signing/encoding.test.ts
+++ src/connect/message-signing/encoding.test.ts
@@ -7,16 +7,17 @@ import { bytesToHex } from 'micro-stacks/common';
 
 describe('encoding', function () {
   test('compat', () => {
-    const message = stacksEncodeMessage('hello');
-    const myMessage = encodeMessage('hello');
+    const message = stacksEncodeMessage('hi there');
+    const myMessage = encodeMessage('hi there');
     expect(bytesToHex(message)).toEqual(bytesToHex(myMessage));
   });
-});
-
-describe('hashing', function () {
-  test('compat', () => {
-    const message = stacksHashMessage('hello');
-    const myMessage = hashMessage('hello');
-    expect(bytesToHex(message)).toEqual(bytesToHex(myMessage));
+  test('message from wallet', () => {
+    const hash = bytesToHex(hashMessage('hi there'));
+    expect(hash).toEqual('9b96a1fe1d548cbbc960cc6a0286668fd74a763667b06366fb2324269fcabaa4');
+  });
+  test('hashing', () => {
+    const message = bytesToHex(stacksHashMessage('hi there'));
+    const myMessage = bytesToHex(hashMessage('hi there'));
+    expect(message).toEqual(myMessage);
   });
 });
