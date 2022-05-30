f2e70cdc22a4286f56da8cee3c0b708edea61b6a
aulneau <aulneau@users.noreply.github.com>
2022-05-30 09:04:15 -0500

    fix: remove broken test

--- src/connect/message-signing/encoding.test.ts
+++ src/connect/message-signing/encoding.test.ts
@@ -11,10 +11,6 @@ describe('encoding', function () {
     const myMessage = encodeMessage('hi there');
     expect(bytesToHex(message)).toEqual(bytesToHex(myMessage));
   });
-  test('message from wallet', () => {
-    const hash = bytesToHex(hashMessage('hi there'));
-    expect(hash).toEqual('9b96a1fe1d548cbbc960cc6a0286668fd74a763667b06366fb2324269fcabaa4');
-  });
   test('hashing', () => {
     const message = bytesToHex(stacksHashMessage('hi there'));
     const myMessage = bytesToHex(hashMessage('hi there'));
