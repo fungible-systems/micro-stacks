c0cd4ad98aab8b2bd9ac42604827f402ff3a93c8
aulneau <aulneau@users.noreply.github.com>
2022-07-01 10:31:17 -0500

    fix: revert to correct impl

--- src/connect/message-signing/structured-message.ts
+++ src/connect/message-signing/structured-message.ts
@@ -1,14 +1,14 @@
 import {
   ClarityValue,
-  hexToCV,
+  cvToHex,
   serializeCV,
   stringAsciiCV,
   tupleCV,
   uintCV,
 } from 'micro-stacks/clarity';
-import { bytesToHex, ChainID, concatByteArrays } from 'micro-stacks/common';
+import { ChainID, concatByteArrays } from 'micro-stacks/common';
 import { Json } from 'micro-stacks/crypto';
-import { openSignMessagePopup } from '../popup';
+import { openSignStructuredDataPopup } from '../popup';
 import type { SignedOptionsWithOnHandlers, StructuredSignatureRequestOptions } from './types';
 import { sha256 } from '@noble/hashes/sha256';
 import { safeGetPublicKey } from '../common/utils';
@@ -35,14 +35,8 @@ export const makeStructuredDataHash = (
 export const generateSignStructuredDataPayload = async (
   options: StructuredSignatureRequestOptions
 ) => {
-  // this is a temp hack to support the latest SIP018 implementation
-  // where the app needs to pass the domain information
-  // in addition to the structured message
-  // the hiro wallet will correctly sign this using the `signMessage` functionality
-  // not the structured message hook they have exposed via StacksProvider
-  // once the team implements the changes, this will be reverted
-  const message: ClarityValue =
-    typeof options.message === 'string' ? hexToCV(options.message) : options.message;
+  const message: string =
+    typeof options.message !== 'string' ? cvToHex(options.message) : options.message;
 
   const domainTuple = makeDomainTuple(
     options.domain.name,
@@ -50,11 +44,12 @@ export const generateSignStructuredDataPayload = async (
     options.domain.chainId ?? options.network?.chainId ?? ChainID.Mainnet
   );
 
-  const hash = makeStructuredDataHash(makeClarityHash(domainTuple), makeClarityHash(message));
+  const domain: string = cvToHex(domainTuple);
 
   const payload: Json = {
     stxAddress: options.stxAddress,
-    message: bytesToHex(hash),
+    message,
+    domain,
     appDetails: options.appDetails,
     publicKey: safeGetPublicKey(options.privateKey),
     network: options.network as any,
@@ -76,8 +71,8 @@ export const handleSignStructuredDataRequest = async (
       appDetails: options.appDetails,
       network: options.network,
     });
-    // TODO: openSignStructuredDataPopup when hiro fixes implementation
-    return openSignMessagePopup({
+
+    return openSignStructuredDataPopup({
       token,
       onFinish: options.onFinish,
       onCancel: options.onCancel,
