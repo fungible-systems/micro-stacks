b62b0a01db76c8599d85dcf124b021ae2e2a4e2a
aulneau <aulneau@users.noreply.github.com>
2022-07-10 09:26:00 -0500

    refactor: remove files

--- packages/svelte/src/status.ts
+++
@@ -1,4 +0,0 @@
-import { getStatus, watchStatus } from '@micro-stacks/client';
-import { readableClientState } from './utils';
-
-export const watchStatuses = readableClientState(getStatus, watchStatus);
