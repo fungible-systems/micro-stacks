d4c42c19034c134cae61b353ff20890800de6043
aulneau <aulneau@users.noreply.github.com>
2022-07-11 17:34:00 -0500

    chore(build): improve build outputs

---
+++ configs/tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  splitting: true,
+  treeshake: true,
+  clean: true,
+  target: 'node16',
+  external: [
+    'micro-stacks/api',
+    'micro-stacks/clarity',
+    'micro-stacks/common',
+    'micro-stacks/connect',
+    'micro-stacks/crypto',
+    'micro-stacks/crypto-extras',
+    'micro-stacks/network',
+    'micro-stacks/storage',
+    'micro-stacks/transactions',
+    'micro-stacks/wallet-sdk',
+    'micro-stacks/zone-file',
+  ],
+  dts: true,
+  minify: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+});
