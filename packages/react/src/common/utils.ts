7b884541ff5888505e30d468458180e62a75be8d
aulneau <aulneau@users.noreply.github.com>
2022-07-20 13:16:51 -0500

    chore(react): try to fix this bug

--- packages/react/src/common/utils.ts
+++ packages/react/src/common/utils.ts
@@ -1,5 +1,11 @@
 import { useMicroStacksClient } from '../hooks/use-client';
-import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/shim/with-selector';
+
+// import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/shim/with-selector'
+// This doesn't work in ESM, because use-sync-external-store only exposes CJS.
+// See: https://github.com/pmndrs/valtio/issues/452
+// The following is a workaround until ESM is supported.
+import useSyncExternalStoreExports from 'use-sync-external-store/shim/with-selector.js';
+
 import type { MicroStacksClient, State } from '@micro-stacks/client';
 
 /** ------------------------------------------------------------------------------------------------------------------
@@ -13,6 +19,8 @@ type GetterFn<V> = (options: { client: MicroStacksClient; state?: State }) => V;
  *   clientStateHookFactory
  *  ------------------------------------------------------------------------------------------------------------------
  */
+
+const { useSyncExternalStoreWithSelector } = useSyncExternalStoreExports;
 
 export function clientStateHookFactory<V>(selector: GetterFn<V>): () => V {
   return () => {
