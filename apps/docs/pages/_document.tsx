c8bbc338d133c9541831178f158319af36c58b64
aulneau <aulneau@users.noreply.github.com>
2022-07-20 10:18:10 -0500

    feat: more docs work

---
+++ apps/docs/pages/_document.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin={'true`'}
+          />
+          <link
+            rel="preconnect"
+            href="https://fungible.systems"
+            crossOrigin={'true`'}
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
