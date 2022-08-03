8c2a09dabafa30de130a91a3d515daa6a7696799
M. Bagher Abiat <zorofight94@gmail.com>
2022-08-03 12:43:28 +0430

    first try

---
+++ packages/core/vitest.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+// @ts-ignore
+import { compilerOptions } from './tsconfig.json';
+
+const aliases = {};
+for (const [key, value] of Object.entries(compilerOptions.paths)) {
+  aliases[key] = path.resolve(process.cwd(), value[0]);
+}
+console.log(aliases)
+
+export default defineConfig(() => {
+  return {
+  test: {
+    environment: 'node',
+    include: ['**/?(*.)+(test).(js|ts|tsx)'],
+    setupFiles: ['./tests/setup.js'],
+    globals: true,
+    alias: aliases,
+    passWithNoTests: true,
+  },
+}
+
+});
