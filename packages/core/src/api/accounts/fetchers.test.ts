a890cf801b31408a871f7e221e867ea48bed20a8
M. Bagher Abiat <zorofight94@gmail.com>
2022-08-03 18:10:20 +0430

    update

--- packages/core/src/api/accounts/fetchers.test.ts
+++ packages/core/src/api/accounts/fetchers.test.ts
@@ -17,6 +17,17 @@ import {
 import { addressEndpoint } from '../utils';
 import { HIRO_TESTNET_DEFAULT } from 'micro-stacks/network';
 
+// convert json modules to js objects
+const ADDRESS_TRANSACTION_WITH_TRANSFERS_RESPONSE_OBJ = JSON.parse(
+  JSON.stringify(ADDRESS_TRANSACTION_WITH_TRANSFERS_RESPONSE)
+);
+const ADDRESS_NONCES_RESPONSE_OBJ = JSON.parse(JSON.stringify(ADDRESS_NONCES_RESPONSE));
+const ADDRESS_STX_INBOUND_LIST_RESPONSE_OBJ = JSON.parse(
+  JSON.stringify(ADDRESS_STX_INBOUND_LIST_RESPONSE)
+);
+const ADDRESS_NFT_LIST_RESPONSE_OBJ = JSON.parse(JSON.stringify(ADDRESS_NFT_LIST_RESPONSE));
+const ACCOUNT_DATA_RESPONSE_OBJ = JSON.parse(JSON.stringify(ACCOUNT_DATA_RESPONSE));
+
 const principal = '1C56LYirKa3PFXFsvhSESgDy2acEHVAEt6';
 const tx_id = '0x5e9f3933e358df6a73fec0d47ce3e1062c20812c129f5294e6f37a8d27c051d9';
 
@@ -24,23 +35,23 @@ export const GET_ACCOUNTS_MOCKS = [
   rest.get(
     `${addressEndpoint(HIRO_TESTNET_DEFAULT)}/${principal}/${tx_id}/transactions_with_transfers`,
     (_req, res, ctx) => {
-      return res(ctx.json(ADDRESS_TRANSACTION_WITH_TRANSFERS_RESPONSE));
+      return res(ctx.json(ADDRESS_TRANSACTION_WITH_TRANSFERS_RESPONSE_OBJ));
     }
   ),
   rest.get(`${addressEndpoint(HIRO_TESTNET_DEFAULT)}/${principal}/nonces`, (_req, res, ctx) => {
-    return res(ctx.json(ADDRESS_NONCES_RESPONSE));
+    return res(ctx.json(ADDRESS_NONCES_RESPONSE_OBJ));
   }),
   rest.get(
     `${addressEndpoint(HIRO_TESTNET_DEFAULT)}/${principal}/stx_inbound`,
     (_req, res, ctx) => {
-      return res(ctx.json(ADDRESS_STX_INBOUND_LIST_RESPONSE));
+      return res(ctx.json(ADDRESS_STX_INBOUND_LIST_RESPONSE_OBJ));
     }
   ),
   rest.get(`${addressEndpoint(HIRO_TESTNET_DEFAULT)}/${principal}/nft_events`, (_req, res, ctx) => {
-    return res(ctx.json(ADDRESS_NFT_LIST_RESPONSE));
+    return res(ctx.json(ADDRESS_NFT_LIST_RESPONSE_OBJ));
   }),
   rest.get(`${addressEndpoint(HIRO_TESTNET_DEFAULT)}/${principal}`, (_req, res, ctx) => {
-    return res(ctx.json(ACCOUNT_DATA_RESPONSE));
+    return res(ctx.json(ACCOUNT_DATA_RESPONSE_OBJ));
   }),
 ];
 
@@ -61,30 +72,30 @@ describe('accounts fetchers', () => {
   test(fetchAccountTransactionWithTransfers.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, principal, tx_id };
     const data = await fetchAccountTransactionWithTransfers(args);
-    expect(data).toEqual(ADDRESS_TRANSACTION_WITH_TRANSFERS_RESPONSE);
+    expect(data).toEqual(ADDRESS_TRANSACTION_WITH_TRANSFERS_RESPONSE_OBJ);
   });
 
   test(fetchAccountNonces.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, principal };
     const data = await fetchAccountNonces(args);
-    expect(data).toEqual(ADDRESS_NONCES_RESPONSE);
+    expect(data).toEqual(ADDRESS_NONCES_RESPONSE_OBJ);
   });
 
   test(fetchAccountStxInbound.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, principal };
     const data = await fetchAccountStxInbound(args);
-    expect(data).toEqual(ADDRESS_STX_INBOUND_LIST_RESPONSE);
+    expect(data).toEqual(ADDRESS_STX_INBOUND_LIST_RESPONSE_OBJ);
   });
 
   test(fetchAccountNftEvents.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, principal };
     const data = await fetchAccountNftEvents(args);
-    expect(data).toEqual(ADDRESS_NFT_LIST_RESPONSE);
+    expect(data).toEqual(ADDRESS_NFT_LIST_RESPONSE_OBJ);
   });
 
   test(fetchAccountInfo.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, principal };
     const data = await fetchAccountInfo(args);
-    expect(data).toEqual(ACCOUNT_DATA_RESPONSE);
+    expect(data).toEqual(ACCOUNT_DATA_RESPONSE_OBJ);
   });
 });
--- packages/core/src/api/tokens/fetchers.test.ts
+++ packages/core/src/api/tokens/fetchers.test.ts
@@ -4,6 +4,20 @@ import * as NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE from '../../../tests/m
 import * as NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE from '../../../tests/mocks/api/tokens/NonFungibleTokenMetadata200.json';
 import * as FUNGIBLE_TOKEN_METADATA_200_RESPONSE from '../../../tests/mocks/api/tokens/FungibleTokenMetadata200.json';
 
+// convert json modules to js objects
+const FUNGIBLE_TOKEN_METADATA_200_RESPONSE_OBJ = JSON.parse(
+  JSON.stringify(FUNGIBLE_TOKEN_METADATA_200_RESPONSE)
+);
+const NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE_OBJ = JSON.parse(
+  JSON.stringify(NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE)
+);
+const FUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE_OBJ = JSON.parse(
+  JSON.stringify(FUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE)
+);
+const NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE_OBJ = JSON.parse(
+  JSON.stringify(NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE)
+);
+
 import { rest } from 'msw';
 import {
   fetchFtMetadataList,
@@ -18,24 +32,24 @@ const contractId = '1C56LYirKa3PFXFsvhSESgDy2acEHVAEt6';
 
 export const GET_TOKEN_MOCKS = [
   rest.get(tokensEndpoint(HIRO_TESTNET_DEFAULT) + '/ft/metadata', (_req, res, ctx) => {
-    return res(ctx.json(FUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE));
+    return res(ctx.json(FUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE_OBJ));
   }),
 
   rest.get(tokensEndpoint(HIRO_TESTNET_DEFAULT) + '/nft/metadata', (_req, res, ctx) => {
-    return res(ctx.json(NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE));
+    return res(ctx.json(NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE_OBJ));
   }),
 
   rest.get(
     tokensEndpoint(HIRO_TESTNET_DEFAULT) + '/' + contractId + '/nft/metadata',
     (_req, res, ctx) => {
-      return res(ctx.json(NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE));
+      return res(ctx.json(NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE_OBJ));
     }
   ),
 
   rest.get(
     tokensEndpoint(HIRO_TESTNET_DEFAULT) + '/' + contractId + '/ft/metadata',
     (_req, res, ctx) => {
-      return res(ctx.json(FUNGIBLE_TOKEN_METADATA_200_RESPONSE));
+      return res(ctx.json(FUNGIBLE_TOKEN_METADATA_200_RESPONSE_OBJ));
     }
   ),
 ];
@@ -57,24 +71,24 @@ describe('tokens fetchers', () => {
   test(fetchFtMetadataList.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT };
     const data = await fetchFtMetadataList(args);
-    expect(data).toEqual(FUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE);
+    expect(data).toEqual(FUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE_OBJ);
   });
 
   test(fetchNftMetadataList.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT };
     const data = await fetchNftMetadataList(args);
-    expect(data).toEqual(NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE);
+    expect(data).toEqual(NONFUNGIBLE_TOKENS_METADATA_LIST_200_RESPONSE_OBJ);
   });
 
   test(fetchNftMetadataForContractId.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, contractId: contractId };
     const data = await fetchNftMetadataForContractId(args);
-    expect(data).toEqual(NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE);
+    expect(data).toEqual(NON_FUNGIBLE_TOKEN_METADATA_200_RESPONSE_OBJ);
   });
 
   test(fetchFtMetadataForContractId.name, async () => {
     const args = { url: HIRO_TESTNET_DEFAULT, contractId: contractId };
     const data = await fetchFtMetadataForContractId(args);
-    expect(data).toEqual(FUNGIBLE_TOKEN_METADATA_200_RESPONSE);
+    expect(data).toEqual(FUNGIBLE_TOKEN_METADATA_200_RESPONSE_OBJ);
   });
 });
