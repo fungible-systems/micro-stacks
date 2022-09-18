c8c167ae21be83c84747316cc1e8bc07557e7f78
aulneau <aulneau@users.noreply.github.com>
2022-09-18 12:27:00 -0500

    feat: update signed message prefix

--- packages/core/src/connect/message-signing/encoding.ts
+++ packages/core/src/connect/message-signing/encoding.ts
@@ -3,7 +3,8 @@ import { decode, encode, encodingLength } from './varuint';
 import { utf8ToBytes } from '@noble/hashes/utils';
 import { concatByteArrays } from 'micro-stacks/common';
 
-const chainPrefix = '\x18Stacks Message Signing:\n';
+const chainPrefix = '\x17Stacks Signed Message:\n';
+const chainPrefixBytes = utf8ToBytes(chainPrefix);
 
 export function hashMessage(message: string | Uint8Array) {
   return hashSha256(encodeMessage(message));
@@ -12,13 +13,11 @@ export function hashMessage(message: string | Uint8Array) {
 export function encodeMessage(message: string | Uint8Array): Uint8Array {
   const bytes = typeof message === 'string' ? utf8ToBytes(message) : message;
   const encoded = encode(bytes.length);
-  return concatByteArrays([utf8ToBytes(chainPrefix), encoded, bytes]);
+  return concatByteArrays([chainPrefixBytes, encoded, bytes]);
 }
 
 export function decodeMessage(encodedMessage: Uint8Array): Uint8Array {
-  // Remove the chain prefix: 1 for the varint and 24 for the length of the string
-  // 'Stacks Message Signing:\n'
-  const messageWithoutChainPrefix = encodedMessage.subarray(1 + 24);
+  const messageWithoutChainPrefix = encodedMessage.subarray(chainPrefixBytes.byteLength);
   const decoded = decode(messageWithoutChainPrefix);
   const varIntLength = encodingLength(decoded);
   // Remove the varuint prefix
