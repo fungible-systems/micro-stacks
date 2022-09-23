fc3e8935c3648130eacdab2532061162db81a0c3
aulneau <aulneau@users.noreply.github.com>
2022-09-23 10:25:31 -0500

    fix: support new and old, tests

--- packages/core/src/connect/message-signing/encoding.ts
+++ packages/core/src/connect/message-signing/encoding.ts
@@ -3,21 +3,29 @@ import { decode, encode, encodingLength } from './varuint';
 import { utf8ToBytes } from '@noble/hashes/utils';
 import { concatByteArrays } from 'micro-stacks/common';
 
-const chainPrefix = '\x17Stacks Signed Message:\n';
-const chainPrefixBytes = utf8ToBytes(chainPrefix);
+const CHAIN_PREFIX = '\x17Stacks Signed Message:\n';
+export const CHAIN_PREFIX_BYTES = utf8ToBytes(CHAIN_PREFIX);
+const LEGACY_CHAIN_PREFIX = '\x18Stacks Message Signing:\n';
+export const LEGACY_CHAIN_PREFIX_BYTES = utf8ToBytes(LEGACY_CHAIN_PREFIX);
 
-export function hashMessage(message: string | Uint8Array) {
-  return hashSha256(encodeMessage(message));
+export function hashMessage(message: string | Uint8Array, prefix: Uint8Array = CHAIN_PREFIX_BYTES) {
+  return hashSha256(encodeMessage(message, prefix));
 }
 
-export function encodeMessage(message: string | Uint8Array): Uint8Array {
+export function encodeMessage(
+  message: string | Uint8Array,
+  prefix: Uint8Array = CHAIN_PREFIX_BYTES
+): Uint8Array {
   const bytes = typeof message === 'string' ? utf8ToBytes(message) : message;
   const encoded = encode(bytes.length);
-  return concatByteArrays([chainPrefixBytes, encoded, bytes]);
+  return concatByteArrays([prefix, encoded, bytes]);
 }
 
-export function decodeMessage(encodedMessage: Uint8Array): Uint8Array {
-  const messageWithoutChainPrefix = encodedMessage.subarray(chainPrefixBytes.byteLength);
+export function decodeMessage(
+  encodedMessage: Uint8Array,
+  prefix: Uint8Array = CHAIN_PREFIX_BYTES
+): Uint8Array {
+  const messageWithoutChainPrefix = encodedMessage.subarray(prefix.byteLength);
   const decoded = decode(messageWithoutChainPrefix);
   const varIntLength = encodingLength(decoded);
   // Remove the varuint prefix
